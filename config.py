e3de5188271135d6e7e536370df69ffa2ca3aa07
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-26 03:58:38 +0000

    last commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'uzimaki12'
-SQLALCHEMY_DATABASE_URI = "postgresql://aymoulppovwwsa:sSy454ybcFo0we4iuBJZMMiVvR@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d3d1fcam2h5fgm"
+SQLALCHEMY_DATABASE_URI = "postgresql://uwpsqxbjlaxvpc:z-lFkC_0QaPfx_12efrqa9ahiQ@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d3h24ba0ni3gjs"
