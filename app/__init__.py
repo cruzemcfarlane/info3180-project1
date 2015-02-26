91e99de280fa65edd26a9ba12b3e788a300607b5
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-26 03:22:33 +0000

    db change commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from app import views, models
 
 app = Flask(__name__)
 app.config.from_object('config')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
 db = SQLAlchemy(app)
 
-from app import views, models
