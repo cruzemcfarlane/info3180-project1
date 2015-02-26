88191287dd21ba73e986e4d98d1ed04b2873e839
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-26 03:52:43 +0000

    last commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from config import SQLALCHEMY_DATABASE_URI
 from flask.ext.sqlalchemy import SQLAlchemy
-from . import views, models
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+from app import views
 
