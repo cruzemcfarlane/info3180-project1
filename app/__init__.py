9bcd75f578f243b00888525a98bc486b5a07b545
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-26 03:27:39 +0000

    db change commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from config import SQLALCHEMY_DATABASE_URI
 from flask.ext.sqlalchemy import SQLAlchemy
-from app import views, models
+from . import views, models
 
 app = Flask(__name__)
 app.config.from_object('config')
--- db_create.py
+++ db_create.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
 from config import SQLALCHEMY_DATABASE_URI
-from app import db, models
+from . import db, models
 
 db.create_all()
