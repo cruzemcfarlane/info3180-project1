939bc23ce2a1547aa77ddb47246729254a97d62b
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-26 02:47:45 +0000

    db change commit

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ SECRET_KEY = 'uzimaki12'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://action@localhost/action' #'postgresql:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgres://yidgzpgztnnjva:gNNKEu-8V_7ZCpMEQMtX4VncHn@ec2-50-19-236-178.compute-1.amazonaws.com:5432/d7gp302hlmnuc5'
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
